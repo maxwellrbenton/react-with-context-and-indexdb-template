db62fb94a57af772df3cdd41af90f8c9da932b67
Maxwell Benton <maxwellbenton@gmail.com>
2019-10-12 21:25:07 -0400

    sets up basic React app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [state, setState] = useState({ count: 0 })
+
+  function handleOnClick(event) {
+    setState({
+      count: state.count + 1
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>{state.count}</h3>
+      <button onClick={handleOnClick}>Add One</button>
     </div>
   );
 }
